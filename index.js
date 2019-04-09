6c3ba23465ee16634a2176e3c072bdb874fd0784
Sandro Brognara <abrognara97@gmail.com>
2019-04-08 20:44:25 -0400

    added listen on different ports other than 3000

--- index.js
+++ index.js
@@ -210,6 +210,6 @@ app.get('/reset', function(req, res) {
  *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Listening!');
 });
